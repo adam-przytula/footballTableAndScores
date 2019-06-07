57a34d53ed91cf409008e52c66c692dee9ea1362
Adam <adprzytula@gmail.com>
2019-06-07 23:33:46 +0200

    Created layout of application

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import javax.swing.text.html.ImageView;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Controller{
+    public ImageView btnAddScore,btnTable,btnHistory;
+
+
+}
